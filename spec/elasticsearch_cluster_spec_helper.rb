b3004cea394ae71578b71254281a3e76485a86e3
Tristan Starck <tstarck@invoca.com>
2024-07-15 11:19:45 -0700

    bump versions

--- spec/elasticsearch_cluster_spec_helper.rb
+++ spec/elasticsearch_cluster_spec_helper.rb
@@ -28,11 +28,20 @@ module ElasticsearchClusterSpecHelper
 
   def clear_and_create_index(index: ELASTICSEARCH_TEST_INDEX)
     if @elasticsearch_test_client&.indices&.exists?(index: index)
-      @elasticsearch_test_client.indices.delete(index: index)
-    end
-
-    unless @elasticsearch_test_client&.indices&.exists?(index: index)
-      @elasticsearch_test_client.indices.create(index: index)
+      @elasticsearch_test_client.indices.refresh(index: index)
+      @elasticsearch_test_client.delete_by_query(index: index, body: {
+        query: {
+          match_all: {}
+        }
+      })
+      @elasticsearch_test_client.indices.refresh(index: index)
+    else
+      @elasticsearch_test_client.indices.create(index: index, timeout: "5s", body: {
+        settings: {
+          number_of_shards: 1,
+          number_of_replicas: 0
+        }
+      })
     end
   end
 
